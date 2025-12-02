079315303ae051717508640a1ac1b4492fdbdf88
LeKhang-cn22h <chube02082017@gmail.com>
2025-12-02 21:58:20 +0700

    create widget home_page

--- lib/views/home/widgets/Card_shop.dart
+++ lib/views/home/widgets/Card_shop.dart
@@ -1,7 +1,9 @@
 import 'package:barbergofe/core/constants/color.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CardShop extends StatelessWidget {
+  final String id;
   final String name;
   final String location;
   final double rank;
@@ -9,6 +11,7 @@ class CardShop extends StatelessWidget {
 
   const CardShop({
     super.key,
+    required this.id,
     required this.imagePath,
     required this.name,
     required this.location,
@@ -53,13 +56,15 @@ class CardShop extends StatelessWidget {
                         Text(location),
                       ]),
                       Row(children: [
-                        const Icon(Icons.star, color: Color(0xFFF3B51C)),
+                         const Icon(Icons.star, color: Color(0xFFF3B51C)),
                         const SizedBox(width: 4),
                         Text(rank.toString(), style: TextStyle(
                           fontWeight: FontWeight.bold
                         ),),
                       ]),
-                      TextButton(onPressed: (){}, child: Text("Xem chi tiết"), style: TextButton.styleFrom(
+                      TextButton(onPressed: (){
+                        context.goNamed('detail', pathParameters: {'id': id});
+                      }, child: Text("Xem chi tiết"), style: TextButton.styleFrom(
                         backgroundColor: AppColors.primary,
                         foregroundColor: AppColors.textPrimaryLight,
 
@@ -71,13 +76,13 @@ class CardShop extends StatelessWidget {
 
               // RIGHT SIDE IMAGE (nhô lên)
               Transform.translate(
-                offset: const Offset(0, -20), // ảnh nhô lên 20px
+                offset: const Offset(0, -30), // ảnh nhô lên 20px
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(12),
                   child: Image.asset(
                     imagePath,
-                    width: 140,
-                    height: 140,
+                    width: 150,
+                    height: 150,
                     fit: BoxFit.cover,
                   ),
                 ),
--- lib/views/home/widgets/FeaturedBarbersSection.dart
+++ lib/views/home/widgets/FeaturedBarbersSection.dart
@@ -1,35 +1,51 @@
 import 'package:barbergofe/core/theme/text_styles.dart';
 import 'package:barbergofe/views/home/widgets/Card_shop.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 
-class Featuredbarberssection extends StatelessWidget {
+class Featuredbarberssection extends StatefulWidget {
   const Featuredbarberssection({super.key});
+
+  @override
+  State<Featuredbarberssection> createState() => _FeaturedbarberssectionState();
+}
+
+class _FeaturedbarberssectionState extends State<Featuredbarberssection> {
+  List<dynamic> barbers=[];
+  @override
+  void initState() {
+    super.initState();
+    loadBarbers();
+  }
+  Future<void> loadBarbers() async {
+    final String jsonString = await rootBundle.loadString('assets/data/barbers.json');
+    final List<dynamic> jsonData = json.decode(jsonString);
+
+    // chỉ lấy 3 barber nổi bật đầu tiên
+    setState(() {
+      barbers = jsonData.take(2).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Text("Barber nổi bật", style: AppTextStyles.heading,),
-        CardShop(
-          name: "Barber 1",
-          location: "Hà Nội",
-          rank: 4.5,
-          imagePath: "assets/images/logos/logoIntro.png",
+        const SizedBox(height: 12,),
+        barbers.isEmpty ? const Center(child: CircularProgressIndicator(),): Column(
+          children: barbers.map((barber){
+            return Column(
+              children: [
+                const SizedBox(height: 50,),
+                CardShop(id: barber['id'],imagePath: barber['imagePath'], name: barber['name'], location: barber['location'], rank: barber['rank']),
+              ],
+            );
+          }
+          ).toList(),
         ),
-        const SizedBox(height: 24,),
-        CardShop(
-          name: "Barber 1",
-          location: "HCM",
-          rank: 4.7,
-          imagePath: "assets/images/logos/logoIntro.png",
-        ),
-        const SizedBox(height: 24,),
-        CardShop(
-          name: "Barber 1",
-          location: "HCM",
-          rank: 4.7,
-          imagePath: "assets/images/logos/logoIntro.png",
-        ),
+
       ],
     );
   }
