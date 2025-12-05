a1dfbef29ac73a902b50d66c1a0b29e1e5f92ce5
LeKhang-cn22h <chube02082017@gmail.com>
2025-12-05 20:29:25 +0700

    chore(app): update MultiProvider & theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,24 @@
 import 'package:barbergofe/core/theme/app_theme.dart';
 import 'package:barbergofe/routes/app_router.dart';
+import 'package:barbergofe/viewmodels/auth/auth_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:barbergofe/core/constants/app_strings.dart';
+import 'package:provider/provider.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (_) => AuthViewModel()..init()),
+    ],
+      child:
+      MaterialApp.router(
       title: CommonStrings.appName,
       debugShowCheckedModeBanner: false,
-      routerConfig: appRouter,
+      routerConfig: AppRouter.router,
       theme: AppTheme.lightTheme,
+    )
     );
   }
 }
