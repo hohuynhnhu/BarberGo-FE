af2a67539603994774bb629e8bd6bd47ad999b5f
LeKhang-cn22h <chube02082017@gmail.com>
2025-12-05 20:29:13 +0700

    chore(config): update API config

--- lib/core/utils/api_config.dart
+++ lib/core/utils/api_config.dart
@@ -3,7 +3,7 @@ class ApiConfig {
   // ==================== BASE URLS ====================
 
   /// Development URL (local/test)
-  static const String devBaseUrl = "http://192.168.1.3:8000";
+  static const String devBaseUrl = "http://192.168.1.11:8000";
 
   // /// Production URL (deploy lên server thật)
   // static const String prodBaseUrl = "https://api.yourdomain.com";
@@ -18,11 +18,45 @@ class ApiConfig {
   static const String acneHistory = "/acne/history";
   static const String acneStats = "/acne/statistics";
 
+  ///auth
+  static const String authRegister= "/users/register";
+  static const String authResent= "/users/resend-confirmation";
+  static const String authLogin = "/users/login";
+  static const String authForgot ="/users/forgot-password";
+  static const String authReset = "/users/reset-password";
+
+  /// GET /users/
+  static const String usersList = "/users/";
+
+  /// GET /users/{id}
+  static const String usersGetById = "/users";
+
+  /// PUT /users/{id}
+  static const String usersUpdate = "/users";
+
+  /// DELETE /users/{id}
+  static const String usersDelete = "/users";
+
+
+
   // ==================== HELPERS ====================
 
   /// Get full URL
   static String getUrl(String endpoint) => '$baseUrl$endpoint';
 
+  static String getUrlWithId(String endpoint, String id) {
+    return '$baseUrl$endpoint/$id';
+  }
+  //     getUrlWithId("/users/profile", "123")
+  //     → "http://192.168.1.11:8000/users/profile/123"
+
+  static Map<String, String> getHeaders({String? token}) {
+    final headers = {'Content-Type': 'application/json'};
+    if (token != null) {
+      headers['Authorization'] = 'Bearer $token';
+    }
+    return headers;
+  }
   /// Timeout durations
   static const Duration timeout = Duration(seconds: 30);
   static const Duration connectionTimeout = Duration(seconds: 15);
