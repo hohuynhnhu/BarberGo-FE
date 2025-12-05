5373df55e7fc50f4b5625d2a111410fee432c707
LeKhang-cn22h <chube02082017@gmail.com>
2025-12-05 20:29:15 +0700

    chore(router): update route names

--- lib/routes/route_names.dart
+++ lib/routes/route_names.dart
@@ -1,5 +1,6 @@
 class RouteNames{
   static const getStarted = '/';
+  static const String intro = '/intro';
   static const home ='/home';
   static const signin = '/signin';
   static const signup = '/signup';
@@ -11,4 +12,9 @@ class RouteNames{
   static const  newPass='/newPass';
   static const acnes = '/acne';
   static const detail='/detail/:id';
+  static const personal='/personal';
+  static const changePass='/ChangePass';
+//EROR
+  static const String notFound = '/404';
+
 }
